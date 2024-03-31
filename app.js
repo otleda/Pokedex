5dcf4c3be24569315ccfaa779d393d10f5285ce3
Adelto Almeida <adtguitar@gmail.com>
2024-03-31 17:33:35 -0300

    pegando os tipos dos pokemos e os Ids, separando as responsabilidades.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
+//Fn 01
 const getTypeColor = type => {
-const normal = '#F5F5F5'
+  const normal = '#F5F5F5'
   return {
     normal,
     fire: '#FDDFDF',
@@ -14,28 +14,45 @@ const normal = '#F5F5F5'
     poison: '#98D7A5',
     bug: '#F8D5A3',
     ghost: '#CAC0F7',
-    dragon: '#97B3E6',
     psychic: '#EAEDA1',
+    dragon: '#97B3E6',
     fighting: '#E6E0D4'
   }[type] || normal
 }
 
-const handlePageloader = async () => {
+//Fn 02 Types to Pokemons
+const getPokemonsType = async pokeApiResults => {
+    const promises = pokeApiResults.map(result => fetch(result.url))
+    const responses = await Promise.allSettled(promises)
+    const fulfilled = responses.filter(response => response.status === 'fulfilled')
+    const pokePromises = fulfilled.map(url => url.value.json())
+    const pokemons = await Promise.all(pokePromises)
+    return pokemons.map(fulfilled => fulfilled.types.map(info => info.type.name))
+  }
+
+// Fn 03
+const getPokemonsIds = (pokeApiResults) => pokeApiResults.map(({ url }) => {
+    const urlArray = url.split('/')
+    return urlArray.at(urlArray.length - 2)
+  })
+
+  const handlePageloader = async () => {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0')
 
     if(!response.ok) {
       throw Error('Não foi possível obter as informações!')
     }
-
-    //Used Destructuring
     const { results : pokeApiResults } =  await response.json()
-    const promises = pokeApiResults.map(result => fetch(result.url))
+    const types = await getPokemonsType(pokeApiResults)    
+    const ids = getPokemonsIds(pokeApiResults)
+
+    const promises = ids.map(id => fetch(`/assets/img/${id}.png`))
     const responses = await Promise.allSettled(promises)
     const fulfilled = responses.filter(response => response.status === 'fulfilled')
-
-
-    console.log(fulfilled);
+    const imgs = fulfilled.map((response) => {return response.value.url})
+    
+    console.log(ids, types, promises, imgs);    
 
   } catch (error) {
     console.log('algo deu errado!');
